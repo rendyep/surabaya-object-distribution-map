1af47dd917906d8bb351ef18bed7b3d5da49d9f1
rendyep <rendyekoprastiyo@gmail.com>
2014-05-21 08:05:54 +0700

    initial commit

---
+++ demo/index.php
@@ -0,0 +1,20 @@
+<?php
+
+include __DIR__ . '/../lib/autoload.php';
+
+?>
+<!DOCTYPE HTML>
+<html lang="id">
+    <head>
+        <title>Surabaya Object Distribution Map Demo</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="css/default.css" />
+        <script type="text/javascript" src="js/kinetic-v5.1.0.min.js"></script>
+        <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+        <script type="text/javascript" src="js/default.js"></script>
+    </head>
+    <body>
+        Hello world!
+    </body>
+</html>
