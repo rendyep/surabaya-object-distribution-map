4ff104bce7ee3f1a725ad5b8122e91fe377a12a6
rendyep <rendyekoprastiyo@gmail.com>
2014-05-22 23:37:16 +0700

    add ajax based data

---
+++ demo/data.php
@@ -0,0 +1,13 @@
+<?php
+
+$data = array(
+    'gresik' => 80,
+    'sidoarjo' => 70,
+    'surabayaBarat' => 90,
+    'surabayaSelatan' => 60,
+    'surabayaTimur' => 40,
+    'surabayaUtara' => 50,
+    'surabayaTengah' => 100
+);
+
+echo json_encode($data);
