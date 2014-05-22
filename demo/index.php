0bf62cbe56ab7b94087cd0e1df6e1a20d3525897
rendyep <rendyekoprastiyo@gmail.com>
2014-05-22 23:39:22 +0700

    update complete functionality of core script

--- demo/index.php
+++ demo/index.php
@@ -4,17 +4,15 @@ include __DIR__ . '/../lib/autoload.php';
 
 ?>
 <!DOCTYPE HTML>
-<html lang="id">
+<html>
     <head>
-        <title>Surabaya Object Distribution Map Demo</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico" />
+        <link rel="shortcut icon" href="img/favicon.ico" />
         <link rel="stylesheet" type="text/css" href="css/default.css" />
-        <script type="text/javascript" src="js/kinetic-v5.1.0.min.js"></script>
-        <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
-        <script type="text/javascript" src="js/default.js"></script>
+        <script src="js/jquery-1.11.1.min.js"></script>
     </head>
-    <body>
-        Hello world!
-    </body>
+  <body>
+        <div id="container"></div>
+        <script src="js/kinetic-v5.1.0.min.js"></script>
+        <script defer="defer" src="js/default.js"></script>
+  </body>
 </html>
