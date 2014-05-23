06d77ad918d1701c65fd8d2f7e510636958fae9a
rendyep <rendyekoprastiyo@gmail.com>
2014-05-23 17:05:26 +0700

    add tooltip

--- demo/data.php
+++ demo/data.php
@@ -1,13 +1,48 @@
 <?php
 
+$total = 1000000;
+$gresik = 100000;
+$sidoarjo = 150000;
+$surabayaBarat = 200000;
+$surabayaTimur = 100000;
+$surabayaSelatan = 150000;
+$surabayaUtara = 50000;
+$surabayaTengah = 250000;
+
 $data = array(
-    'gresik' => 80,
-    'sidoarjo' => 70,
-    'surabayaBarat' => 90,
-    'surabayaSelatan' => 60,
-    'surabayaTimur' => 40,
-    'surabayaUtara' => 50,
-    'surabayaTengah' => 100
+    'global' => array(
+        'population' => $total,
+    ),
+    'area' => array(
+        'gresik' => array(
+            'population' => $gresik,
+            'density' => $gresik/$total,
+        ),
+        'sidoarjo' => array(
+            'population' => $sidoarjo,
+            'density' => $sidoarjo/$total,
+        ),
+        'surabayaBarat' => array(
+            'population' => $surabayaBarat,
+            'density' => $surabayaBarat/$total,
+        ),
+        'surabayaSelatan' => array(
+            'population' => $surabayaSelatan,
+            'density' => $surabayaSelatan/$total,
+        ),
+        'surabayaTimur' => array(
+            'population' => $surabayaTimur,
+            'density' => $surabayaTimur/$total,
+        ),
+        'surabayaUtara' => array(
+            'population' => $surabayaUtara,
+            'density' => $surabayaUtara/$total,
+        ),
+        'surabayaTengah' => array(
+            'population' => $surabayaTengah,
+            'density' => $surabayaTengah/$total,
+        ),
+    ),
 );
 
 echo json_encode($data);
--- demo/js/default.js
+++ demo/js/default.js
@@ -10,8 +10,13 @@ var stage = new Kinetic.Stage({
 var layer = new Kinetic.Layer();
 
 var imageObject = new Image();
+imageObject.src = 'img/map.png';
 imageObject.onload = function() {
-    var cityMap = new Kinetic.Image({
+    var density = {
+        'sidoarjo': 100
+    };
+
+    var globalMap = new Kinetic.Image({
         x: 0,
         y: 0,
         image: imageObject,
@@ -19,70 +24,154 @@ imageObject.onload = function() {
         height: stageHeight
     });
 
-    var gresikMap = new Kinetic.Path({
-        x: 0,
-        y: 0,
-        data: 'M 0,0 154,0 154,444 78,444 78,522 0,522 0,0 z',
-        fill: 'black',
-        opacity: 0
+    var areaMap = {
+        'gresik': new Kinetic.Path({
+            x: 0,
+            y: 0,
+            data: 'M 0,0 154,0 154,444 78,444 78,522 0,522 0,0 z',
+            fill: 'black',
+            opacity: 0,
+            stroke: 'white',
+            strokeWidth: '3'
+        }),
+        'sidoarjo': new Kinetic.Path({
+            x: 0,
+            y: 0,
+            data: 'M 0,522 78,522 78,444 154,444 800,444 800,600 0,600 0,522 z',
+            fill: 'black',
+            opacity: 0,
+            stroke: 'white',
+            strokeWidth: '3'
+        }),
+        'surabayaBarat': new Kinetic.Path({
+            x: 0,
+            y: 0,
+            data: 'M 154,0 232,0 232,78 310,78 310,366 232,366 232,444 154,444 154,0 z',
+            fill: 'black',
+            opacity: 0,
+            stroke: 'white',
+            strokeWidth: '3'
+        }),
+        'surabayaSelatan': new Kinetic.Path({
+            x: 0,
+            y: 0,
+            data: 'M 310,286 645,286 645,366 724,366 724,444 232,444 232,366 310,366 310,286 z',
+            fill: 'black',
+            opacity: 0,
+            stroke: 'white',
+            strokeWidth: '3'
+        }),
+        'surabayaTimur': new Kinetic.Path({
+            x: 0,
+            y: 0,
+            data: 'M 724,0 800,0 800,444 724,444 724,366 645,366 645,78 724,78 724,0 z',
+            fill: 'black',
+            opacity: 0,
+            stroke: 'white',
+            strokeWidth: '3'
+        }),
+        'surabayaUtara': new Kinetic.Path({
+            x: 0,
+            y: 0,
+            data: 'M 232,0 724,0 724,78 645,78 645,158 310,158 310,78 232,78 232,0 z',
+            fill: 'black',
+            opacity: 0,
+            stroke: 'white',
+            strokeWidth: '3'
+        }),
+        'surabayaTengah': new Kinetic.Path({
+            x: 0,
+            y: 0,
+            data: 'M 310,158 645,158 645,286 310,286 310,158 z',
+            fill: 'black',
+            opacity: 0,
+            stroke: 'white',
+            strokeWidth: '3'
+        })
+    };
+
+    var areaLabel = {
+        'gresik': {
+            'text': 'Kabupaten Gresik',
+            'element': new Kinetic.Label({
+                x: 75,
+                y: 240
+            })
+        },
+        'sidoarjo': {
+            'text': 'Kabupaten Sidoarjo',
+            'element': new Kinetic.Label({
+                x: 400,
+                y: 530
+            })
+        },
+        'surabayaBarat': {
+            'text': 'Surabaya Barat',
+            'element': new Kinetic.Label({
+                x: 235,
+                y: 240
+            })
+        },
+        'surabayaSelatan': {
+            'text': 'Surabaya Selatan',
+            'element': new Kinetic.Label({
+                x: 470,
+                y: 380
+            })
+        },
+        'surabayaTimur': {
+            'text': 'Surabaya Timur',
+            'element': new Kinetic.Label({
+                x: 720,
+                y: 240
+            })
+        },
+        'surabayaUtara': {
+            'text': 'Surabaya Utara',
+            'element': new Kinetic.Label({
+                x: 470,
+                y: 80
+            })
+        },
+        'surabayaTengah': {
+            'text': 'Surabaya Tengah',
+            'element': new Kinetic.Label({
+                x: 470,
+                y: 240
+            })
+        }
+    };
+
+    $.each(areaLabel, function(key, label){
+        label.element.add(new Kinetic.Tag({
+            fill: 'blue',
+            pointerDirection: 'down',
+            pointerWidth: 10,
+            pointerHeight: 10,
+            lineJoin: 'round',
+            shadowColor: 'navy',
+            shadowBlur: 10,
+            shadowOffset: {
+                x: 10,
+                y: 20
+            },
+            shadowOpacity: 0.5
+        })).add(new Kinetic.Text({
+            text: this.text,
+            fontFamily: 'Calibri',
+            fontSize: 18,
+            padding: 5,
+            fill: 'white'
+        }));
     });
 
-    var sidoarjoMap = new Kinetic.Path({
-        x: 0,
-        y: 0,
-        data: 'M 0,522 78,522 78,444 154,444 800,444 800,600 0,600 0,522 z',
-        fill: 'black',
-        opacity: 0
+    layer.add(globalMap);
+    $.each(areaMap, function(key, map){
+        layer.add(map);
     });
-
-    var surabayaBaratMap = new Kinetic.Path({
-        x: 0,
-        y: 0,
-        data: 'M 154,0 232,0 232,78 310,78 310,366 232,366 232,444 154,444 154,0 z',
-        fill: 'black',
-        opacity: 0
+    $.each(areaLabel, function(key, label){
+        layer.add(label.element);
     });
-
-    var surabayaSelatanMap = new Kinetic.Path({
-        x: 0,
-        y: 0,
-        data: 'M 310,286 645,286 645,366 724,366 724,444 232,444 232,366 310,366 310,286 z',
-        fill: 'black',
-        opacity: 0
-    });
-
-    var surabayaTimurMap = new Kinetic.Path({
-        x: 0,
-        y: 0,
-        data: 'M 724,0 800,0 800,444 724,444 724,366 645,366 645,78 724,78 724,0 z',
-        fill: 'black',
-        opacity: 0
-    });
-
-    var surabayaUtaraMap = new Kinetic.Path({
-        x: 0,
-        y: 0,
-        data: 'M 232,0 724,0 724,78 645,78 645,158 310,158 310,78 232,78 232,0 z',
-        fill: 'black',
-        opacity: 0
-    });
-
-    var surabayaTengahMap = new Kinetic.Path({
-        x: 0,
-        y: 0,
-        data: 'M 310,158 645,158 645,286 310,286 310,158 z',
-        fill: 'black',
-        opacity: 00
-    });
-
-    layer.add(cityMap);
-    layer.add(gresikMap);
-    layer.add(sidoarjoMap);
-    layer.add(surabayaBaratMap);
-    layer.add(surabayaSelatanMap);
-    layer.add(surabayaTimurMap);
-    layer.add(surabayaUtaraMap);
-    layer.add(surabayaTengahMap);
 
     stage.add(layer);
 
@@ -90,15 +179,15 @@ imageObject.onload = function() {
         url: 'data.php'
     }).done(function(data){
         data = $.parseJSON(data);
-        gresikMap.opacity(data.gresik/100);
-        sidoarjoMap.opacity(data.sidoarjo/100);
-        surabayaBaratMap.opacity(data.surabayaBarat/100);
-        surabayaSelatanMap.opacity(data.surabayaSelatan/100);
-        surabayaTimurMap.opacity(data.surabayaTimur/100);
-        surabayaUtaraMap.opacity(data.surabayaUtara/100);
-        surabayaTengahMap.opacity(data.surabayaTengah/100);
+
+        $.each(data.area, function(key, element){
+            areaMap[key].opacity(element.density);
+        });
+
+        $.each(areaLabel, function(key, label){
+            layer.add(label.element);
+        });
+
         layer.draw();
     });
 };
-
-imageObject.src = 'img/map.png';
